2bcfd65662f78d6783a71883a374358ebd04e9e4
Abso <rliu14@jhu.edu>
2015-12-20 20:55:11 -0500

    Documentation for mad libs component classes.

--- src/main/java/com/madlibs/model/MadLibsBlankComponent.java
+++ src/main/java/com/madlibs/model/MadLibsBlankComponent.java
@@ -1,9 +1,27 @@
 package com.madlibs.model;
 
 /**
+ * Blank component, to be filled out by the player, within a mad lib.
  * Created by Ran on 12/20/2015.
  */
 public class MadLibsBlankComponent extends MadLibsComponent {
+
+    /**
+     * Gets the template display value of this component, which is the value of the label assigned to this blank.
+     * @return The label/prompt for this blank.
+     */
+    public String getTemplateDisplayValue() {
+        return null; // TODO
+    }
+
+    /**
+     * Gets the script display value of this component, which is the value of the answer
+     * if there is one, filled out by a player.
+     * @return Answer filled into this blank in the script.
+     */
+    public String getScriptDisplayValue() {
+        return null; // TODO
+    }
 
 
 
--- src/main/java/com/madlibs/model/MadLibsTextComponent.java
+++ src/main/java/com/madlibs/model/MadLibsTextComponent.java
@@ -1,7 +1,24 @@
 package com.madlibs.model;
 
 /**
+ * Text component of a mad lib, with text specified in the template.
  * Created by Ran on 12/20/2015.
  */
 public class MadLibsTextComponent extends MadLibsComponent {
+
+    /**
+     * Returns the template display value of this component.
+     * @return The value of the text within this component.
+     */
+    public String getTemplateDisplayValue() {
+        return null; // TODO
+    }
+
+    /**
+     * Gets the script display value of this component.
+     * @return The value of the text within this component.
+     */
+    public String getScriptDisplayValue() {
+        return null; // TODO
+    }
 }
