959c1658c3b4558b85bd0510ccb710b323ebeb79
Abso <rliu14@jhu.edu>
2015-12-22 14:37:23 -0500

    Model code. Added rest endpoints.

--- src/main/java/com/madlibs/server/MadLibsServer.java
+++ src/main/java/com/madlibs/server/MadLibsServer.java
@@ -6,11 +6,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
+ * Mad libs server object.
  * Created by Ran on 12/22/2015.
  */
 public class MadLibsServer {
 
     private List<MadLibsSession> gameSessions;
+
+    private static MadLibsServer instance = new MadLibsServer();
+
+    public static MadLibsServer getInstance() {
+        return instance;
+    }
 
     /**
      * Creates a new server for handling games.
