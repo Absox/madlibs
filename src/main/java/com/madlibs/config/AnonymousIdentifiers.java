1ad45797893898d7cc768bc76fe5aa510986bff1
Absox <rliu14@jhu.edu>
2015-12-24 22:08:24 -0500

    Added celebrity identifiers for anon users.

---
+++ src/main/java/com/madlibs/config/AnonymousIdentifiers.java
@@ -0,0 +1,63 @@
+package com.madlibs.config;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * Helper class for keeping track of identifier configs for anonymous users.
+ * Created by Ran on 12/24/2015.
+ */
+public class AnonymousIdentifiers {
+
+    private List<String> identifiers;
+
+    private static AnonymousIdentifiers instance;
+
+    static {
+        try {
+            instance = new AnonymousIdentifiers("data/Celebs");
+        } catch (FileNotFoundException e) {
+            System.err.println("Configuration file not found: anonymous identifiers");
+            System.err.println(e.getMessage());
+        }
+    }
+
+    /**
+     * Reads in list of identifiers
+     * @param filename Filename to read from
+     * @throws FileNotFoundException File not found
+     */
+    public AnonymousIdentifiers(String filename) throws FileNotFoundException {
+
+        this.identifiers = new ArrayList<>();
+        Scanner fileScanner = new Scanner(new File(filename));
+
+        while (fileScanner.hasNextLine()) {
+            String currentLine = fileScanner.nextLine();
+            if (currentLine.length() > 0 && !currentLine.startsWith("//")) {
+                identifiers.add(currentLine);
+
+            }
+        }
+        fileScanner.close();
+    }
+
+    /**
+     * Accessor for singleton instance.
+     * @return Singleton instance.
+     */
+    public static AnonymousIdentifiers getInstance() {
+        return instance;
+    }
+
+    /**
+     * Accessor for list of identifiers.
+     * @return List of identifiers.
+     */
+    public List<String> getIdentifiers() {
+        return this.identifiers;
+    }
+}
---
+++ src/test/java/unit/AnonymousIdentifiersTest.java
@@ -0,0 +1,34 @@
+package unit;
+
+import com.madlibs.config.AnonymousIdentifiers;
+import org.junit.Test;
+
+import java.io.FileNotFoundException;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+/**
+ * Unit tests for anonymous identifiers.
+ * Created by Ran on 12/24/2015.
+ */
+public class AnonymousIdentifiersTest {
+
+    @Test
+    public void testFileInput() {
+
+        try {
+            AnonymousIdentifiers identifiers = new AnonymousIdentifiers("testResources/testCelebs");
+            List<String> identifierStrings = identifiers.getIdentifiers();
+            assertEquals(identifierStrings.size(), 3);
+            assertEquals(identifierStrings.get(0), "Nic_Cage");
+            assertEquals(identifierStrings.get(1), "James_Franco");
+            assertEquals(identifierStrings.get(2), "Sean_Bean");
+
+        } catch (FileNotFoundException e) {
+            fail();
+        }
+
+    }
+}
