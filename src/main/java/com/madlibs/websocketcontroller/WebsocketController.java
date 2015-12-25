4bd7c5e259c9f6d9aa82b07691ababa8c36f6afe
Absox <rliu14@jhu.edu>
2015-12-25 09:44:03 -0500

    Added websocket controllers.

--- src/main/java/com/madlibs/websocketcontroller/WebsocketController.java
+++ src/main/java/com/madlibs/websocketcontroller/WebsocketController.java
@@ -1,13 +1,15 @@
 package com.madlibs.websocketcontroller;
 
+import org.eclipse.jetty.websocket.api.Session;
 import org.eclipse.jetty.websocket.api.annotations.OnWebSocketClose;
 import org.eclipse.jetty.websocket.api.annotations.OnWebSocketConnect;
 import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
 import org.eclipse.jetty.websocket.api.annotations.WebSocket;
-import spark.Session;
+
+import java.io.IOException;
 
 /**
- * Websocket controller.
+ * Main websocket controller.
  * Created by Ran on 12/23/2015.
  */
 @WebSocket
@@ -15,17 +17,19 @@ public class WebsocketController {
 
     @OnWebSocketConnect
     public void onConnect(Session session) {
-
+        System.out.println("Session " + session.getLocalAddress() + " connected");
     }
 
     @OnWebSocketMessage
-    public void onMessage(Session session, String message) {
-
+    public void onMessage(Session session, String message) throws IOException {
+        System.out.println("Message received: " + message);
+        System.out.println("Source: " + session.getRemoteAddress());
+        session.getRemote().sendString("Message received!");
     }
 
     @OnWebSocketClose
     public void onClose(Session session, int statusCode, String reason) {
-
+        System.out.println("Session " + session.getLocalAddress() + " disconnected with status " + statusCode + " reason " + reason);
     }
 
 }
