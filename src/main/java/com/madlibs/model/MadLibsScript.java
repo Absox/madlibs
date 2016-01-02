aa2f04455503844c7a5f821309c8552bb7a612e2
Absox <rliu14@jhu.edu>
2016-01-02 09:16:53 -0500

    Added account update endpoint.

---
+++ src/main/java/com/madlibs/model/MadLibsScript.java
@@ -0,0 +1,13 @@
+package com.madlibs.model;
+
+/**
+ * Object representation of a script (completed template)
+ * Created by Ran on 1/2/2016.
+ */
+public class MadLibsScript {
+
+
+    public MadLibsScript() {
+
+    }
+}
